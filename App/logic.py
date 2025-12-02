89ea4e3f06bac50030f554e8e76142700a4d0dac
San-FG <manuelsal5436-gmail.com>
2025-12-01 22:31:53 -0500

    Primera version del logic

--- App/logic.py
+++ App/logic.py
@@ -1,9 +1,24 @@
 import time
+import os 
+import csv
+from DataStructures.List import array_list as al
+from DataStructures.Map import map_linear_probing as lp
+from DataStructures.Graph import digraph as dg
+from math import radians, cos, sin, asin, sqrt
+
+data_dir = os.path.dirname(os.path.realpath('__file__')) + '/Data/Challenge-4'
 
 def new_logic():
     """
     Crea el catalogo para almacenar las estructuras de datos
     """
+    analyzer = {
+    "event" : al.newlist(), # La parte de los elementos organizados por el timestap
+    "events_by_tags" : lp.new_map(40,0.5,None), #Cada una de las grullas con su nodo de referencia
+    "graph_distance" : dg.new_graph(None),
+    "graph_water":dg.new_graph(None)
+    }
+    return analyzer
     #TODO: Llama a las funciónes de creación de las estructuras de datos
     pass
 
@@ -14,9 +29,40 @@ def load_data(catalog, filename):
     """
     Carga los datos del reto
     """
+    catalog["event"] = load_grullas(catalog["event"])
+    
     # TODO: Realizar la carga de datos
     pass
 
+def load_grullas(catalog):
+    flight_file = data_dir + "/1000_cranes_mongolia_large.csv" 
+    input_file = csv.DictReader(open(flight_file, encoding="utf-8"), delimiter=",")
+    
+    for each in input_file:
+        al.add_last(catalog,each)
+
+    def default_sort(a1,a2):
+        if a1["timestamp"] < a2["timestamp"]:
+            return True
+        return False
+    al.merge_sort(catalog,default_sort)
+    return catalog
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
 # Funciones de consulta sobre el catálogo
 
 
